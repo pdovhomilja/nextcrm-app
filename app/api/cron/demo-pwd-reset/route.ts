56caf8f963a40cda757066a2d01d57859de2518d
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-08-08 20:38:09 +0200

    Update - demo-pwd-reset route

--- app/api/cron/demo-pwd-reset/route.ts
+++ app/api/cron/demo-pwd-reset/route.ts
@@ -1,19 +1,26 @@
 import { prismadb } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { hash } from "bcryptjs";
 
 export async function GET(req: Request) {
   //console.log(req.headers.get("host"), "req.headers");
 
   //check if domain is demo.nextcrm.io if yes, reset password to default
   if (req.headers.get("host") === "demo.nextcrm.io") {
+    const password = process.env.DEFAULT_PASSWORD;
+
+    if (!password)
+      return NextResponse.json({
+        message: "demo.nextcrm.io password change to default failed",
+      });
+
     try {
       await prismadb.users.update({
         where: {
           id: "64bb9da09771b681ec8f4a81",
         },
         data: {
-          password:
-            "$2a$10$3W99vHPKrmB/rAhubRzahu4Q2jbnXTvchk2LoYKNWX7TcfVeUZ8YG",
+          password: await hash(password, 12),
           name: "Demo Admin",
           is_admin: true,
           userStatus: "ACTIVE",
