7acc0796fa8f498f70e097fbb6c06f5e3f18ff2b
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-09-18 08:01:16 +0200

    Vercel build fix

--- app/[locale]/(routes)/crm/dashboard/page.tsx
+++ app/[locale]/(routes)/crm/dashboard/page.tsx
@@ -3,7 +3,6 @@ import Container from "../../components/ui/Container";
 import { getSaleStages } from "@/actions/crm/get-sales-stage";
 import CRMKanban from "./_components/CRMKanban";
 import { getOpportunities } from "@/actions/crm/get-opportunities";
-import CRMKanbanServer from "./_components/CRMKanbanServer";
 
 const CrmDashboardPage = async () => {
   const salesStages = await getSaleStages();
