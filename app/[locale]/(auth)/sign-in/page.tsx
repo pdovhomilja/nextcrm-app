b5c8acb651f6cc7c1b5529bee835240d3bd4a5ea
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-10-26 12:00:34 +0200

    Update - Login Screen - fix problem with scroll on small displays

--- app/[locale]/(auth)/sign-in/page.tsx
+++ app/[locale]/(auth)/sign-in/page.tsx
@@ -12,13 +12,15 @@ const SignInPage = async () => {
   } */
 
   return (
-    <div>
+    <div className="h-full overflow-scroll ">
       <div className="py-10">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Welcome to {process.env.NEXT_PUBLIC_APP_NAME}
         </h1>
       </div>
-      <LoginComponent />
+      <div>
+        <LoginComponent />
+      </div>
     </div>
   );
 };
