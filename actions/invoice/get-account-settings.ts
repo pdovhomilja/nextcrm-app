774b42cb6c0991e74e13f3fa13e776a8a71de6a9
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-11-02 18:13:02 +0100

    Fix types 5

--- actions/invoice/get-account-settings.ts
+++ actions/invoice/get-account-settings.ts
@@ -1,5 +1,4 @@
 import { prismadb } from "@/lib/prisma";
-import { MyAccount } from "@prisma/client";
 
 export async function getAccountSettings() {
   const myAccount = await prismadb.myAccount.findFirst({});
