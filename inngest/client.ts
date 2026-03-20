dd65c3e308a34a9980e369d20864738c7e8648e4
Pavel Dovhomilja <pavel@dovhomilja.dev>
2026-03-20 19:31:08 +0100

    fix: inngest

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,3 +1,8 @@
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest({ id: "nextcrm" });
+export const inngest = new Inngest({
+  id: "nextcrm",
+  name: "NextCRM",
+  eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
+});
