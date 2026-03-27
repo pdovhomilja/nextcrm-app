a719bc598bd77245f4a108c48901cf116b8bc2fe
Pavel Dovhomilja <pavel@dovhomilja.dev>
2026-03-27 09:48:20 +0100

    fix: udpate inngest client

--- inngest/client.ts
+++ inngest/client.ts
@@ -1,8 +1,8 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-  id: "nextcrm-dev",
-  name: "NextCRM",
+  id: process.env.INNGEST_ID as string,
+  name: process.env.INNGEST_APP_NAME as string,
   eventKey: process.env.INNGEST_EVENT_KEY,
   signingKey: process.env.INNGEST_SIGNING_KEY,
 });
