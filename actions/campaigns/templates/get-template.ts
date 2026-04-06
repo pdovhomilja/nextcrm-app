a95ac246e784cb3748a676126beefbd6b7e20d37
Pavel -  ClawdBot <pavel-clawdbot@Jana--MacBook-Air.local>
2026-04-06 18:01:42 +0200

    feat: align campaign template actions with deletedAt soft delete

--- actions/campaigns/templates/get-template.ts
+++ actions/campaigns/templates/get-template.ts
@@ -2,8 +2,8 @@
 import { prismadb } from "@/lib/prisma";
 
 export const getTemplate = async (id: string) => {
-  return prismadb.crm_campaign_templates.findUnique({
-    where: { id },
+  return prismadb.crm_campaign_templates.findFirst({
+    where: { id, deletedAt: null },
     include: { created_by_user: { select: { name: true } } },
   });
 };
--- actions/campaigns/templates/get-templates.ts
+++ actions/campaigns/templates/get-templates.ts
@@ -3,6 +3,7 @@ import { prismadb } from "@/lib/prisma";
 
 export const getTemplates = async () => {
   return prismadb.crm_campaign_templates.findMany({
+    where: { deletedAt: null },
     orderBy: { created_on: "desc" },
     include: { created_by_user: { select: { name: true } } },
   });
